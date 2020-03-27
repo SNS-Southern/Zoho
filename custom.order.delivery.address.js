4fbfc74c2ff1f8c58f22c8766652c1a23c2c7d8c
SNS-Southern <62056936+SNS-Southern@users.noreply.github.com>
2020-03-27 09:01:05 +0000

    Add files via upload

---
+++ custom.order.delivery.address.js
@@ -0,0 +1,50 @@
+var recordData={},seid,moduleName;
+$(document).ready(function(){
+ZOHO.embeddedApp.on("PageLoad",function(data){
+    recordData={
+        Entity:data.Entity,
+        RecordID:data.EntityId
+    };
+    seid=recordData.RecordID;
+    moduleName=recordData.Entity;
+    initializewidget();
+        })
+        ZOHO.embeddedApp.init().then(function(){   	
+       		ZOHO.CRM.UI.Resize({height:500,width:1300})
+     });
+});
+function initializewidget(){
+    var arr = [];  
+    ZOHO.CRM.API.getRecord({Entity:"Sales_Orders",RecordID:seid})
+.then(function(resp){
+ZOHO.CRM.API.getRelatedRecords({Entity:"Accounts",RecordID:resp.data[0].Account_Name['id'],RelatedList:"Addresses",page:1,per_page:200})
+.then(function(resp){
+    var addressTemplate =  $('#address-template').html();
+    var addressCompliedTemplate = Handlebars.compile(addressTemplate);
+    $('#table-container').html(addressCompliedTemplate(resp));
+})
+        }) 
+    }
+
+function closewidget(){
+    ZOHO.CRM.UI.Popup.close().then(function(data){console.log(data)})
+}
+function associateaddress(){
+    $('.ui.radio.checkbox input:radio').each(function(){
+        if($(this).is(":checked")){
+        var address_id = $(this).attr('id');
+        var address_name = $(this).attr('data-value');
+           var recordData = {
+               "Delivery_Address_Lookup":{
+                   "id": address_id.toString(),
+                   "name": address_name.toString()
+               }                
+           }
+            ZOHO.CRM.UI.Record.populate(recordData)
+.then(function(data){
+    console.log(data)
+})
+        }
+    });
+    closewidget();
+}
